df80572469c5a6c7654ba31f1713bb57f5132b27
nethushinavodya <nethushiperera@gmail.com>
2026-01-07 19:34:53 +0530

    remove commented-out code for JWT token signing

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,18 +1,3 @@
-// import { IUser } from "../models/userModel"
-// import jwt from "jsonwebtoken"
-// import dotenv from "dotenv"
-// dotenv.config()
-//
-// const JWT_SECRET = process.env.JWT_SECRET as string
-//
-// export const signAccessToken = (user: IUser): string => {
-//   return jwt.sign({ sub: user._id, roles: user.role }, JWT_SECRET, {
-//     expiresIn: "30m"
-//   })
-// }
-//
-// console.log(process.env.JWT_SECRET)
-
 import { IUser } from "../models/userModel"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
